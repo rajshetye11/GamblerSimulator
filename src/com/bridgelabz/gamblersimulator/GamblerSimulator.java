db3f9b57b632b9a61eca10721d5e8f3481a856d5
rajshetye11 <rajshetye11@gmail.com>
2021-08-12 13:03:42 +0530

    [raj] : added whether Gambler won or lost

--- src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
+++ src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
@@ -1,3 +1,5 @@
+
+
 package com.bridgelabz.gamblersimulator;
 
 import java.util.Random;
@@ -10,26 +12,32 @@ public class GamblerSimulator {
 		int start_stake = 100;
 		int bet_per_game = 1;
 		int temp_stake=start_stake;
+		int goal=150;
+		int max_loss=50;
 		
 		Random random = new Random();
-		int option = random.nextInt(2);
+		
 		
 		System.out.println("Starting stake $ "+start_stake);
 		System.out.println("Bet per game $ "+bet_per_game);
-		
-		if(option==0)
-		{
-			System.out.println("Won the Game");
-			temp_stake++;
-		}else
-		{
-			System.out.println("Lost the Game");
-			temp_stake--;
+		do {
+			int option = random.nextInt(2);
+			if(option==0)
+			{
+				System.out.println("Won the Game");
+				start_stake+= 1;
+				System.out.println("Current Stakes "+start_stake);
+			}else
+			{
+				System.out.println("Lost the Game");
+				start_stake-=1;
+				System.out.println("Current Stakes "+start_stake);
+			}
 		}
+		while(start_stake > 50 && start_stake < 150);
 		
+//		System.out.println("Current Stakes "+temp_stake);
 		
-		
-		System.out.println("Current Stakes "+temp_stake);
 
 	}
 }
