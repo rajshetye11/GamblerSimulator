8119c4ef5dd08f13f89a35f93b97901f59475f3e
rajshetye11 <rajshetye11@gmail.com>
2021-08-12 11:21:40 +0530

    [raj] : added initial setup for Gambler Simulator

---
+++ src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.gamblersimulator;
+
+public class GamblerSimulator {
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Gambler Simulator");
+
+	}
+}
