b9a7b0955df8c3e75f000d4b8272ce1d342acf7d
rajshetye11 <rajshetye11@gmail.com>
2021-08-12 11:36:05 +0530

    [raj] : added stakes of Gambler

--- src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
+++ src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
@@ -4,6 +4,12 @@ public class GamblerSimulator {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Gambler Simulator");
+		
+		int start_stake = 100;
+		int bet_per_game = 1;
+		
+		System.out.println("Starting stake $ "+start_stake);
+		System.out.println("Bet per game $ "+bet_per_game);
 
 	}
 }
