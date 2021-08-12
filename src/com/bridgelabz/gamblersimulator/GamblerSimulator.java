ffe84d5c245c8ba796664cd1ebbf5feef81e4094
rajshetye11 <rajshetye11@gmail.com>
2021-08-12 17:16:43 +0530

    [raj] : added whether Gambler won or lost

--- src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
+++ src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
@@ -1,26 +1,18 @@
-
-
 package com.bridgelabz.gamblersimulator;
 
 import java.util.Random;
 
 public class GamblerSimulator {
-	public static void main(String[] args) {
-
-		System.out.println("Welcome to Gambler Simulator");
-		
-		int start_stake = 100;
-		int bet_per_game = 1;
-		int temp_stake=start_stake;
-		int goal=150;
-		int max_loss=50;
-		
+	
+	public static int start_stake = 100;
+	public static int bet_per_game = 1;
+	int goal=150;
+	int max_loss=50;
+	
+	public void winOrLoss() {
 		Random random = new Random();
 		
-		
-		System.out.println("Starting stake $ "+start_stake);
-		System.out.println("Bet per game $ "+bet_per_game);
-		do {
+		while(start_stake > max_loss && start_stake < goal) {
 			int option = random.nextInt(2);
 			if(option==0)
 			{
@@ -33,10 +25,15 @@ public class GamblerSimulator {
 				start_stake-=1;
 				System.out.println("Current Stakes "+start_stake);
 			}
-		}
-		while(start_stake > 50 && start_stake < 150);
-		
-		
-
+		}	
+	}
+	
+	public static void main(String[] args) {
+		GamblerSimulator Uc3 = new GamblerSimulator();
+	
+		System.out.println("Welcome to Gambler Simulator");
+		System.out.println("Starting stake $ "+start_stake);
+		System.out.println("Bet per game $ "+bet_per_game);
+		Uc3.winOrLoss();
 	}
 }
