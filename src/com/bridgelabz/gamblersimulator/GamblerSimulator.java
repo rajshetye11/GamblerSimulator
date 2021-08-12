2d7508380c846edbadd103aac8a7be2f9ec26c50
rajshetye11 <rajshetye11@gmail.com>
2021-08-12 12:11:28 +0530

    [raj] : added bet feature

--- src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
+++ src/com/bridgelabz/gamblersimulator/GamblerSimulator.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.gamblersimulator;
 
+import java.util.Random;
+
 public class GamblerSimulator {
 	public static void main(String[] args) {
 
@@ -7,9 +9,27 @@ public class GamblerSimulator {
 		
 		int start_stake = 100;
 		int bet_per_game = 1;
+		int temp_stake=start_stake;
+		
+		Random random = new Random();
+		int option = random.nextInt(2);
 		
 		System.out.println("Starting stake $ "+start_stake);
 		System.out.println("Bet per game $ "+bet_per_game);
+		
+		if(option==0)
+		{
+			System.out.println("Won the Game");
+			temp_stake++;
+		}else
+		{
+			System.out.println("Lost the Game");
+			temp_stake--;
+		}
+		
+		
+		
+		System.out.println("Current Stakes "+temp_stake);
 
 	}
 }
